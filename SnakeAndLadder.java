ac7473dd0f7dfe9e8c5505c243beb6934f2605bf
trampboy495 <histroylover@gmail.com>
2020-08-20 14:53:47 +0530

    added: cases for snake, ladder and no play

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,10 +3,23 @@ import java.util.Random;
 public class SnakeAndLadder{
 	static int noOfDiceRolls=0;
 	static int positionOfPlayer=0;
+	static final int isSnake = 2;
+	static final int isLadder = 1;
+	static final int isNoPlay = 0;
 
 	public static void playGame(){
 		Random rand = new Random();
 		int diceRoll = rand.nextInt(6) + 1;
+		int checkCase = rand.nextInt(3);
+		switch(checkCase){
+			case isLadder :
+				positionOfPlayer += diceRoll;
+				break;
+			case isSnake :
+				positionOfPlayer -= diceRoll;
+				break;
+		}
+		System.out.println(positionOfPlayer);
 	}
 
 	public static void main(String[]args){
