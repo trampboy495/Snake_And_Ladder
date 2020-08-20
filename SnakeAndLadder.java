8aebdb9f92ba031819a6a483a8f15e64d041e0cd
trampboy495 <histroylover@gmail.com>
2020-08-20 16:37:36 +0530

    added: game for two players to play

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,37 +1,84 @@
 import java.util.Random;
 
 public class SnakeAndLadder{
-	static int noOfDiceRolls=0;
-	static int positionOfPlayer=0;
+	static int noOfDiceRolls = 0;
+	static int noOfDiceRollsPlayer1 = 0;
+	static int noOfDiceRollsPlayer2 = 0;
+	static int positionOfPlayer1 = 0;
+	static int positionOfPlayer2 = 0;
 	static final int isSnake = 2;
 	static final int isLadder = 1;
 	static final int isNoPlay = 0;
+	static int isPlayerOne = 1;
+	static int isPlayerTwo = 2;
+	static int whooseTurn = isPlayerOne;
+
 
 	public static void playGame(){
-		System.out.print("Dice Roll \t Player Position \n");
-		while(positionOfPlayer < 100){
+		System.out.print("Dice Roll \t Player1 Position \t Player2 Position \n");
+		while(positionOfPlayer1 < 100 && positionOfPlayer2 < 100){
 			Random rand = new Random();
 			int diceRoll = rand.nextInt(6) + 1;
 			int checkCase = rand.nextInt(3);
-			switch(checkCase){
-				case isLadder :
-					positionOfPlayer += diceRoll;
-					break;
-				case isSnake :
-					positionOfPlayer -= diceRoll;
-					break;
-			}
 			noOfDiceRolls++;
-			if(positionOfPlayer < 0){
-				positionOfPlayer = 0;
-			}
-			else if(positionOfPlayer > 100){
-				positionOfPlayer -= diceRoll;
-			}
-			System.out.print(noOfDiceRolls+" \t\t "+positionOfPlayer+" \n");
-
+			updatePosition(whooseTurn , checkCase , diceRoll);
+			System.out.print(noOfDiceRolls+" \t\t "+positionOfPlayer1+" \t\t\t "+positionOfPlayer2+"  \n");
 		}
-		System.out.println("No of dice rolls required to win "+noOfDiceRolls);
+		if(positionOfPlayer1 == 100){
+			System.out.println("Player 1 won and No of dice rolls required to win "+noOfDiceRollsPlayer1);
+		}
+		else{
+			System.out.println("Player 2 won and No of dice rolls required to win "+noOfDiceRollsPlayer2);
+		}
+	}
+
+	public static void updatePosition(int player, int checkCase , int diceRoll){
+		switch(checkCase){
+			case isLadder :
+				if(player == isPlayerOne){
+					positionOfPlayer1 += diceRoll;
+					noOfDiceRollsPlayer1++;
+					if(positionOfPlayer1 > 100){
+						positionOfPlayer1 -= diceRoll;
+					}
+				}
+				else{
+					positionOfPlayer2 += diceRoll;
+					noOfDiceRollsPlayer2++;
+					if(positionOfPlayer2 > 100){
+						positionOfPlayer2 -= diceRoll;
+					}
+				}
+				break;
+			case isSnake :
+				if(player == isPlayerOne){
+					positionOfPlayer1 -= diceRoll;
+				}
+				else{
+					positionOfPlayer2 -= diceRoll;
+				}
+				changeTurn(player);
+				break;
+			default:
+				changeTurn(player);
+			}
+	}
+
+	public static void changeTurn(int player){
+		if(player == isPlayerOne){
+			noOfDiceRollsPlayer1++;
+			if(positionOfPlayer1 < 0){
+				positionOfPlayer1 = 0;
+			}
+			whooseTurn = isPlayerTwo;
+		}
+		else{
+			noOfDiceRollsPlayer2++;
+			if(positionOfPlayer2 < 0){
+				positionOfPlayer2 = 0;
+			}
+			whooseTurn = isPlayerOne;
+		}
 	}
 
 	public static void main(String[]args){
