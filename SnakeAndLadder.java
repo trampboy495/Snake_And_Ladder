d76c7a0ac68640609b16d6563568a7e9aae14408
trampboy495 <histroylover@gmail.com>
2020-08-20 15:06:12 +0530

    added: iterating till player reaches 100

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -8,18 +8,22 @@ public class SnakeAndLadder{
 	static final int isNoPlay = 0;
 
 	public static void playGame(){
-		Random rand = new Random();
-		int diceRoll = rand.nextInt(6) + 1;
-		int checkCase = rand.nextInt(3);
-		switch(checkCase){
-			case isLadder :
-				positionOfPlayer += diceRoll;
-				break;
-			case isSnake :
-				positionOfPlayer -= diceRoll;
-				break;
+		while(positionOfPlayer < 100 ){
+			Random rand = new Random();
+			int diceRoll = rand.nextInt(6) + 1;
+			int checkCase = rand.nextInt(3);
+			switch(checkCase){
+				case isLadder :
+					positionOfPlayer += diceRoll;
+					break;
+				case isSnake :
+					positionOfPlayer -= diceRoll;
+					break;
+			}
+			if(positionOfPlayer < 0 ){
+				positionOfPlayer = 0;
+			}
 		}
-		System.out.println(positionOfPlayer);
 	}
 
 	public static void main(String[]args){
