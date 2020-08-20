5228da7842b1ccfb17040396fb295083e104c983
trampboy495 <histroylover@gmail.com>
2020-08-20 14:37:32 +0530

    added: generated random dice roll

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -4,7 +4,12 @@ public class SnakeAndLadder{
 	static int noOfDiceRolls=0;
 	static int positionOfPlayer=0;
 
-	public static void main(String[]args){
+	public static void playGame(){
+		Random rand = new Random();
+		int diceRoll = rand.nextInt(6) + 1;
+	}
 
+	public static void main(String[]args){
+		playGame();
 	}
 }
